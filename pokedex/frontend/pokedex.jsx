5444c2823db042098dcc7a9a86f9dc5f8520a332
jli57 <jingna13@gmail.com>
2018-10-03 14:38:40 -0700

    Add frontend file structure

---
+++ pokedex/frontend/pokedex.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById("root");
+  // const store
+  ReactDOM.render(<h1>Pokedex</h1>, root);
+}); 
